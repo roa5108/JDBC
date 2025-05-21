cfe7b92b49b1b7e273eb4db7f28cba57ec8f2bc2
Roa Kim <roa5108@gmail.com>
2025-05-21 14:37:43 +0900

    0521

---
+++ src/test/java/org/scoula/jdbc_ex/dao/UserDaoImplTest.java
@@ -0,0 +1,28 @@
+package org.scoula.jdbc_ex.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoImplTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getList() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
